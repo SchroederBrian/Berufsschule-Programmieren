612d3e7b93e4a62ad66b1a8279e355aecc82a62b
Brian Schröder <schroeder.brian@gmx.de>
2023-12-01 10:29:59 +0100

    FahrzeitMitPAP.java erstellt anhand PAP Design aus dem Unterricht

---
+++ src/Fahrkartenautomat/FahrzeitMitPAP.java
@@ -0,0 +1,63 @@
+package Fahrkartenautomat;
+
+import java.util.Scanner;
+
+public class FahrzeitMitPAP {
+    public static void main(String[] args) throws InterruptedException {
+        int fahrzeit;
+        String eingabe;
+
+        Scanner tastatur = new Scanner(System.in);
+
+        System.out.println("Du startest in Berlin Hbf. und fährst nach Spandau.");
+        fahrzeit = 8;
+
+        System.out.println("Möchtest du in Spandau halten?");
+        eingabe = tastatur.nextLine();
+
+        if (eingabe.equalsIgnoreCase("ja") || eingabe.equalsIgnoreCase("yes") || eingabe.equalsIgnoreCase("true") || eingabe.equals("1")) {
+           fahrzeit = fahrzeit + 2;
+        }
+
+        System.out.println("Möchtest du nach Hamburg fahren?");
+        eingabe = tastatur.nextLine();
+        if (eingabe.equalsIgnoreCase("ja") || eingabe.equalsIgnoreCase("yes") || eingabe.equalsIgnoreCase("true") || eingabe.equals("1")) {
+            fahrzeit = fahrzeit + 96;
+            System.out.println("Der Zug endet in Hamburg\n");
+        }else {
+            fahrzeit = fahrzeit + 34;
+
+            System.out.println("Der zug fährt weiter richtung Stendal. Möchtest du Stendal Umfahren?");
+            eingabe = tastatur.nextLine();
+            if (eingabe.equalsIgnoreCase("ja") || eingabe.equalsIgnoreCase("yes") || eingabe.equalsIgnoreCase("true") || eingabe.equals("1")) {
+                fahrzeit = fahrzeit + 6;
+            }else {
+                fahrzeit = fahrzeit + 16;
+            }
+
+            System.out.println("Möchtest du nach Wolfsburg?");
+            eingabe = tastatur.nextLine();
+            if (eingabe.equalsIgnoreCase("ja") || eingabe.equalsIgnoreCase("yes") || eingabe.equalsIgnoreCase("true") || eingabe.equals("1")) {
+                System.out.println("Der Zug endet in Wolfsburg\n");
+                fahrzeit = fahrzeit + 29;
+            }else {
+                System.out.println("Möchtest du nach Braunschweig?");
+                eingabe = tastatur.nextLine();
+                if (eingabe.equalsIgnoreCase("ja") || eingabe.equalsIgnoreCase("yes") || eingabe.equalsIgnoreCase("true") || eingabe.equals("1")) {
+                    System.out.println("Der Zug endet in Braunschweig\n");
+                    fahrzeit = fahrzeit + 50;
+                }else {
+                    System.out.println("Du möchtest also nach Hannover. Der Zug endet in Hannover\n");
+                    fahrzeit = fahrzeit + 62;
+                }
+            }
+        }
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print("=");
+            Thread.sleep(200);
+        }
+
+        System.out.println("\nDeine Fahrzeit beträgt: " + fahrzeit + "min");
+    }
+}
