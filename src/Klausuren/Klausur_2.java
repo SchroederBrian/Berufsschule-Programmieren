00a456a58c3500efe12df1c7058578357d0e7d8b
Notebook <schroeder.brian@gmx.de>
2023-12-20 12:31:08 +0100

    edited Klausur_2.java

--- src/Klausuren/Klausur_2.java
+++
@@ -1,9 +0,0 @@
-package Klausuren;
-
-public class Klausur_2 {
-    public static void main(String[] args) {
-        //DINGES SCRIPT FÜR KLAUSUR PROGRAMMIER AUFGABE
-
-
-    }
-}
---
+++ src/Klausuren/Wettkampf.java
@@ -0,0 +1,38 @@
+package Klausuren;
+
+import java.util.Scanner;
+
+public class Wettkampf  {
+    public static void main(String[] args) {
+        int punkteSpieler1 = 0;
+        int punkteSpieler2 = 0;
+        int punkteSatzgewinn = 15;
+        int spieler;
+
+        Scanner tastatur = new Scanner(System.in);
+
+        while (punkteSpieler1 < 15 || punkteSpieler2 < 15 ) {
+
+            System.out.println("Bitte geben Sie \"1\" falls Spieler 1 ein Punkt erhält oder \"2\" falls Spieler 2 den Punkt erhält: ");
+
+            spieler = tastatur.nextInt();
+
+            if (spieler == 1) {
+                punkteSpieler1++;
+            } else if (spieler == 2) {
+                punkteSpieler2++;
+            } else {
+                System.out.println("Falsche Eingabe!");
+            }
+
+            if (punkteSpieler1 >= punkteSatzgewinn) {
+                System.out.println("Satzgewinn Spieler 1");
+                System.exit(0);
+            } else if (punkteSpieler2 >= punkteSatzgewinn) {
+                System.out.println("Satzgewinn Spieler 2");
+                System.exit(0);
+            }
+        }
+    }
+}
+
