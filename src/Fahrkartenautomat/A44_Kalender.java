08a22790a7d8c642bb9b766cf6ad29fc561002f8
Notebook <schroeder.brian@gmx.de>
2023-12-20 10:53:13 +0100

    editiert a4.4

--- src/Fahrkartenautomat/A44_Kalender.java
+++ src/Fahrkartenautomat/A44_Kalender.java
@@ -15,62 +15,62 @@ public class A44_Kalender {
             case 1:
                 //januar
                 tage = 31;
-                ausgabe(tage, 1);
+                ausgabe(tage, 1, "Januar");
                 break;
             case 2:
                 //februar
                 tage = 28;
-                ausgabe(tage, 2);
+                ausgabe(tage, 2, "Februar");
                 break;
             case 3:
                 //märz
                 tage = 31;
-                ausgabe(tage, 3);
+                ausgabe(tage, 3, "März");
                 break;
             case 4:
                 //april
                 tage = 30;
-                ausgabe(tage, 4);
+                ausgabe(tage, 4, "April");
                 break;
             case 5:
                 //mai
                 tage = 31;
-                ausgabe(tage, 5);
+                ausgabe(tage, 5, "Mai");
                 break;
             case 6:
                 //juni
                 tage = 30;
-                ausgabe(tage, 6);
+                ausgabe(tage, 6, "Juni");
                 break;
             case 7:
                 //juli
                 tage = 31;
-                ausgabe(tage, 7);
+                ausgabe(tage, 7, "Juli");
                 break;
             case 8:
                 //august
                 tage = 31;
-                ausgabe(tage, 8);
+                ausgabe(tage, 8, "August");
                 break;
             case 9:
                 //september
                 tage = 30;
-                ausgabe(tage, 9);
+                ausgabe(tage, 9, "September");
                 break;
             case 10:
                 //october
                 tage = 31;
-                ausgabe(tage, 10);
+                ausgabe(tage, 10, "October");
                 break;
             case 11:
                 //november
                 tage = 30;
-                ausgabe(tage, 11);
+                ausgabe(tage, 11, "November");
                 break;
             case 12:
                 //dezember
                 tage = 31;
-                ausgabe(tage, 12);
+                ausgabe(tage, 12, "Dezember");
                 break;
             default:
                 System.out.println("Die eingegebene zahl ist kein gültiger Monat, bitte starte das Programm neu und wähle von 1-12 aus.");
@@ -79,7 +79,7 @@ public class A44_Kalender {
         tastatur.close();
     }
 
-    private static void ausgabe(int tage, int monat) {
-        System.out.printf("Der %d-te Monat im Jahr hat %d Tage.", monat, tage);
+    private static void ausgabe(int tage, int monat, String name) {
+        System.out.printf("Der %d-te Monat im Jahr hat %d Tage. Der Name des Monats lautet: %s", monat, tage, name);
     }
 }
